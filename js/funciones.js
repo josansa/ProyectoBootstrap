9f8a1ccd31baaa436cd05ac1c1260b1a9b22e1fb
josansa <josansacr7@gmail.com>
2022-03-05 14:09:20 +0100

    menu y form fix

--- js/funciones.js
+++ js/funciones.js
@@ -1,23 +1,14 @@
 let botones = document.getElementsByTagName("button");
-botonActivoInicio = document.getElementById("nav-inicio-tab").setAttribute("class", "nav-link border-0 link-activo")
 for (let i = 0; i < botones.length; i++) {
   botones[i].addEventListener("click", activar);
 }
 
 function activar() {
   for (let i = 0; i < botones.length; i++) {
-    if (botones[i].id != "menu") {
+    if (botones[i].id != "menu" && botones[i].id != "enviar") {
       if (botones[i].id == this.id) {
-        botones[i].ariaSelected = true;
-        let id = botones[i].id;
-        let mostrar = document.getElementById(id.slice(0, -4));
-        let clase = mostrar.className + " show active";
-        mostrar.setAttribute("class", clase);
-        botones[i].setAttribute("class", "nav-link border-0 link-activo");
+        this.setAttribute("class", "nav-link border-0 link-activo");
       } else {
-        botones[i].ariaSelected = false;
-        let ocultar = document.getElementById(botones[i].id.slice(0, -4));
-        ocultar.setAttribute("class", "tab-pane fade");
         botones[i].setAttribute("class", "nav-link border-0");
       }
     }
